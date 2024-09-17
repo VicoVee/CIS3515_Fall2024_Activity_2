e142f2bb63f3192ea6433d986b1b588a365ea02c
Victoria <tovictoriadao@gmail.com>
2024-09-17 14:53:42 -0400

    Populated the numberArray array with values from 5 to 100

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -1,11 +1,15 @@
 package edu.temple.inclassuiacvitivity
 
 import android.content.ContentValues.TAG
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,16 +19,30 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* TODO Step 2: Populate this array with multiples of 5 from 5 - 100*/
-        val numberArray = arrayListOf<Int>()
-        numberArray.add(5)
-        Log.d("TAG", "$numberArray")
+        val numberArray = Array(20, {(it + 1) * 5})
+        Log.d("TAG", numberArray.joinToString( ", "))
 
         /* TODO Step 3: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: OnItemSelectedListener,
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                p0?.run{
+                    //Get the color from the spinner by calling the Adapter
+                    //and getting the selected index
+                    val color = getItemAtPosition(p2).toString()
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+            override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
 
     }
 }
